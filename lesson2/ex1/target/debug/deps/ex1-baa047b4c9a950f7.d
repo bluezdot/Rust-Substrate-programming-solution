361e3ebed512dc6f87a99fd3df3b244a3c8042f6
bluezdot <thanhtruong27701@gmail.com>
2022-06-16 14:50:53 +0700

    add ex1 lesson2

---
+++ lesson2/ex1/target/debug/deps/ex1-baa047b4c9a950f7.d
@@ -0,0 +1,5 @@
+/home/truongpham/rust/Rust-Substrate-programming-solution/lesson2/ex1/target/debug/deps/ex1-baa047b4c9a950f7: src/main.rs
+
+/home/truongpham/rust/Rust-Substrate-programming-solution/lesson2/ex1/target/debug/deps/ex1-baa047b4c9a950f7.d: src/main.rs
+
+src/main.rs:
---
+++ lesson2/ex1/target/debug/ex1.d
@@ -0,0 +1,1 @@
+/home/truongpham/rust/Rust-Substrate-programming-solution/lesson2/ex1/target/debug/ex1: /home/truongpham/rust/Rust-Substrate-programming-solution/lesson2/ex1/src/main.rs
