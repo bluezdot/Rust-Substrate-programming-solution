00c1986053541bb5b7707c37a948e50170242030
bluezdot <72647326+bluezdot@users.noreply.github.com>
2022-06-23 11:18:45 +0700

    Delete lesson2/ex1/target directory

--- lesson2/ex1/target/debug/deps/ex1-baa047b4c9a950f7.d
+++
@@ -1,5 +0,0 @@
-/home/bluezdot/rust/Rust-Substrate-programming-solution/lesson2/ex1/target/debug/deps/ex1-baa047b4c9a950f7: src/main.rs
-
-/home/bluezdot/rust/Rust-Substrate-programming-solution/lesson2/ex1/target/debug/deps/ex1-baa047b4c9a950f7.d: src/main.rs
-
-src/main.rs:
--- lesson2/ex1/target/debug/ex1.d
+++
@@ -1,1 +0,0 @@
-/home/bluezdot/rust/Rust-Substrate-programming-solution/lesson2/ex1/target/debug/ex1: /home/bluezdot/rust/Rust-Substrate-programming-solution/lesson2/ex1/src/main.rs
